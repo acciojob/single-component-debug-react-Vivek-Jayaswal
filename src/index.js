7250e6abbd9b0dfc7cedc74f3b35fbb49957a5d0
Vivek-Jayaswal <141914455+Vivek-Jayaswal@users.noreply.github.com>
2024-06-03 09:34:45 +0000

    seacond push

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import ReactDOM from "react-dom/client";
+import App from "./components/App.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App/>);
